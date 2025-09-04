3b68a2dd29d1ac6025a378c7db43adcae4362fd4
Khaled Zakarya <khaledzz1062003@gmail.com>
2025-09-04 03:40:57 +0300

    correction version 0.1 -- creation version 0.2

--- src/main.py
+++ src/main.py
@@ -1,14 +1,14 @@
 from services.quiz_service import QuizService
 
 if __name__ == "__main__":
-    pdf_path = "data\The Machine Learning Pipeline.pdf"
+    pdf_path = "data/The Machine Learning Pipeline.pdf"
     service = QuizService(pdf_path)
 
     quiz = service.generate_quiz(
-        focus_pages=[2, 3],
-        remain_pages=[1, 4, 5],
-        n_focus=40,
-        n_remain=20,
+        focus_pages=[3],
+        remain_pages=[1,2],
+        n_focus=5,
+        n_remain=10,
         f_mcq_ratio=0.6,
         f_tf_ratio=0.2,
         f_written_ratio=0.2,
